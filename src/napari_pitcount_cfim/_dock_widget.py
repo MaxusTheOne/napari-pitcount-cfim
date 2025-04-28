ed9d762974163464e707cc7aa94dc1c3cffda57a
MaxusTheOne <43919485+MaxusTheOne@users.noreply.github.com>
2025-04-28 14:19:04 +0200

    Added logo stuff

--- src/napari_pitcount_cfim/_dock_widget.py
+++ src/napari_pitcount_cfim/_dock_widget.py
@@ -1,12 +1,6 @@
-<<<<<<< Updated upstream
-from PyQt5.QtWidgets import QGroupBox
-from qtpy.QtWidgets import QWidget, QVBoxLayout
-=======
 import pathlib
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QLayout, QLabel, QGroupBox
 
-from PyQt5.QtWidgets import QLabel
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QLayout
->>>>>>> Stashed changes
 
 from napari_pitcount_cfim.config.settings_handler import SettingsHandler
 from napari_pitcount_cfim.image_handling.image_handler import ImageHandler
@@ -47,9 +41,9 @@ class MainWidget(QWidget):
         settings = self.setting_handler.get_updated_settings()
 
         self.image_handler.set_output_path(settings.get("input_folder"))
-<<<<<<< Updated upstream
+
         self.result_handler.set_output_path(settings.get("output_folder"))
-=======
+
 
     def _add_logo(self):
         """
@@ -60,4 +54,4 @@ class MainWidget(QWidget):
         logo_label.setText(f"<img src='{path}' width='320'/>")
         print(f"Dev | Logo path: {path}")
         self.layout().addWidget(logo_label)
->>>>>>> Stashed changes
+
