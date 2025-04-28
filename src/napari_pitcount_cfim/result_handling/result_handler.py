2846e8276df8811b10557cbf23680c5cece0cde8
MaxusTheOne <43919485+MaxusTheOne@users.noreply.github.com>
2025-04-28 13:52:07 +0200

    UI + grouping

--- src/napari_pitcount_cfim/result_handling/result_handler.py
+++ src/napari_pitcount_cfim/result_handling/result_handler.py
@@ -1,6 +1,7 @@
 # Python
 from pathlib import Path
-from PyQt5.QtWidgets import QWidget, QFileDialog
+from PyQt5.QtWidgets import QWidget, QFileDialog, QPushButton
+
 
 class ResultHandler(QWidget):
     """
@@ -10,6 +11,10 @@ class ResultHandler(QWidget):
         super().__init__(parent)
         self.output_path = output_path
         self.prompt_for_folder = prompt_for_folder
+        self.skip_output_confirmation = False
+        self.results = {}
+
+        self.output_button = None
 
     def set_output_path(self, path):
         """
@@ -18,6 +23,12 @@ class ResultHandler(QWidget):
         if not isinstance(path, str):
             raise ValueError("Output path must be a string.")
         self.output_path = path
+
+    def init_output_button_ui(self):
+        self.output_button = QPushButton("Get results")
+        self.output_button.clicked.connect(self._output_results)
+
+        return self.output_button
 
     def _select_folder(self) -> bool:
         """
@@ -35,10 +46,16 @@ class ResultHandler(QWidget):
         return True
 
     def output_results(self, results: dict):
+        return self._output_results(results)
+
+    def _output_results(self, results: dict = None):
         """
-        Take results in the form of a dictionary where each key is a name and the value is a result dictionary.
-        Outputs each result as plain text to a separate file named after the key.
-        """
+                Take results in the form of a dictionary where each key is a name and the value is a result dictionary.
+                Outputs each result as plain text to a separate file named after the key.
+                """
+        if results is None:
+            results = self.results
+
         if self.prompt_for_folder:
             if not self._select_folder():
                 return  # user cancelled folder selection
